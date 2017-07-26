b699b3fc10e2fc6d74d497cc10c240ec62e3c118
M <1746809408@qq.com>
2017-07-26 14:38:01 +0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,11 +1,11 @@
 function ajax(method,url,data,async,successfn){
 	var xhr=null;
 	if(window.XMLHttpRequest){
-		xhr=new XMLHttpRequest;
+		xhr=new XMLHttpRequest;// 非ie6
 	}else{
-		xhr=new ActiveXObject('Microsoft.XMLHTTP');
+		xhr=new ActiveXObject('Microsoft.XMLHTTP');// 兼容ie6
 	}
-	xhr.open(method,url+'?'+data,async);
+	xhr.open(method,url,data,async); //异步填 true或者 false
 	if(method=='POST'){
 		xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
 		xhr.send(data);
